9f3b03872ed452b2bcf2c07e6fb1c9ae99fe0c06
Adam Bryt <adam.bryt@gmx.com>
2017-05-06 18:13:09 +0200

    Refaktoring i dodanie formatowania wydruku

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 // 2017-02-04 adbr
 
+// TODO: pakiet dla openweathermap?
+
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -10,6 +13,8 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"text/template"
+	"time"
 )
 
 const (
@@ -31,36 +36,97 @@ func main() {
 	}
 	city := flag.Arg(0)
 
-	// Utworzenie URLa z zapytaniem
-	query := url.Values{
-		"appid": {serviceApiKey},
-		"q":     {city},
-	}
-	urlStr := serviceURL + "?" + query.Encode()
-
-	fmt.Printf("[debug] url: %s\n", urlStr) // debug
-
-	// Request HTTP
-	resp, err := http.Get(urlStr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	// Wczytanie zwróconych danych
-	data, err := ioutil.ReadAll(resp.Body)
+	weather, err := getWeather(city)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Wydruk zwróconych danych
-	fmt.Printf("%s\n", data)
+	err = printWeather(weather)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
+
+const usageStr = `Sposób użycia: pogoda miasto
+`
 
 // usage drukuje na stderr sposób użycia programu.
 func usage() {
 	fmt.Fprint(os.Stderr, usageStr)
 }
 
-const usageStr = `Sposób użycia: pogoda miasto
+// Typ WeatherResult representuje dane pogodowe zdekodowane z JSONa.
+type WeatherResult struct {
+	Name string
+	Main struct {
+		Temp     float64
+		Pressure float64
+		Humidity float64
+		TempMin  float64 `json:"temp_min"`
+		TempMax  float64 `json:"temp_max"`
+	}
+	Wind struct {
+		Speed float64
+	}
+	Clouds struct {
+		All float64
+	}
+	Sys struct {
+		Country     string
+		SunriseUnix int64 `json:"sunrise"`
+		SunsetUnix  int64 `json:"sunset"`
+		Sunrise     time.Time
+		Sunset      time.Time
+	}
+}
+
+// getWeather zwraca pogodę dla miasta city.
+func getWeather(city string) (*WeatherResult, error) {
+	query := url.Values{
+		"appid": {serviceApiKey},
+		"units": {"metric"},
+		"q":     {city},
+	}
+	resp, err := http.Get(serviceURL + "?" + query.Encode())
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Wczytanie zwróconych danych w formacie JSON.
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Dekodowanie JSONa.
+	result := new(WeatherResult)
+	err = json.Unmarshal(data, result)
+	if err != nil {
+		return nil, err
+	}
+	result.Sys.Sunrise = time.Unix(result.Sys.SunriseUnix, 0)
+	result.Sys.Sunset = time.Unix(result.Sys.SunsetUnix, 0)
+	return result, nil
+}
+
+const templStr = `Miasto:	       {{.Name}}, {{.Sys.Country}}
+Temperatura:   {{.Main.Temp}} °C (min: {{.Main.TempMin}}, max: {{.Main.TempMax}})
+Ciśnienie:     {{.Main.Pressure}} hpa
+Wilgotność:    {{.Main.Humidity}} %
+Wiatr:         {{.Wind.Speed}} m/s
+Zachmurzenie:  {{.Clouds.All}} %
+Wschód słońca: {{.Sys.Sunrise}}
+Zachód słońca: {{.Sys.Sunset}}
+[Dane pochodzą z serwisu OpenWeatherMap]
 `
+
+var templ = template.Must(template.New("weather").Parse(templStr))
+
+func printWeather(weather *WeatherResult) error {
+	err := templ.Execute(os.Stdout, weather)
+	if err != nil {
+		return err
+	}
+	return nil
+}
