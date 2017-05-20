5ab02642541e18d0333e78320c9a7ff05394532a
Adam Bryt <adam.bryt@gmx.com>
2017-05-20 19:24:53 +0200

    Dodanie więcej wyświetlanych informacji
    - współrzędne geograficzne
    - opis pogody
    - kierunek wiatru

--- main.go
+++ main.go
@@ -31,7 +31,6 @@ func main() {
 	flag.Parse()
 
 	if flag.NArg() < 1 {
-		log.Print("brakuje nazwy miasta")
 		usage()
 		os.Exit(1)
 	}
@@ -58,7 +57,17 @@ func usage() {
 
 // Typ WeatherResult representuje dane pogodowe zdekodowane z JSONa.
 type WeatherResult struct {
-	Name string
+	Coord struct {
+		Lat float64 // city geo location, latitude
+		Lon float64 // city geo location, longitude
+	}
+	Weather []struct {
+		Id          int
+		Main        string
+		Description string
+		Icon        string
+	}
+	Base string
 	Main struct {
 		Temp     float64
 		Pressure float64
@@ -66,19 +75,28 @@ type WeatherResult struct {
 		TempMin  float64 `json:"temp_min"`
 		TempMax  float64 `json:"temp_max"`
 	}
-	Wind struct {
+	Visibility int
+	Wind       struct {
 		Speed float64
+		Deg   float64
 	}
 	Clouds struct {
 		All float64
 	}
+	Dt  int64
 	Sys struct {
+		Type        int
+		Id          int
+		Message     float64
 		Country     string
-		SunriseUnix int64 `json:"sunrise"`
-		SunsetUnix  int64 `json:"sunset"`
-		SunriseTime string
-		SunsetTime  string
+		SunriseUnix int64  `json:"sunrise"`
+		SunsetUnix  int64  `json:"sunset"`
+		SunriseTime string // sformatowany czas z SunriseUnix
+		SunsetTime  string // sformatowany czas z SunsetUnix
 	}
+	Id   int    // city id
+	Name string // city name
+	Cod  int
 }
 
 // getWeather zwraca pogodę dla miasta city.
@@ -112,11 +130,14 @@ func getWeather(city string) (*WeatherResult, error) {
 	return result, nil
 }
 
-const templStr = `Miasto:	       {{.Name}}, {{.Sys.Country}}
+const templStr = `Miasto:	       {{.Name}}, {{.Sys.Country}} [{{.Coord.Lat}}, {{.Coord.Lon}}]
 Temperatura:   {{.Main.Temp}} °C (min: {{.Main.TempMin}}, max: {{.Main.TempMax}})
+{{range .Weather -}}
+Pogoda:        {{.Main}} ({{.Description}})
+{{- end}}
 Ciśnienie:     {{.Main.Pressure}} hpa
 Wilgotność:    {{.Main.Humidity}} %
-Wiatr:         {{.Wind.Speed}} m/s
+Wiatr:         {{.Wind.Speed}} m/s ({{.Wind.Deg}}°)
 Zachmurzenie:  {{.Clouds.All}} %
 Wschód słońca: {{.Sys.SunriseTime}}
 Zachód słońca: {{.Sys.SunsetTime}}
